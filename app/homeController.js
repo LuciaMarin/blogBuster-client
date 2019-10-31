5c1219ef7db05109b43ed8ca1f66678b0dcc9e56
MARIN CORONAS  LUCIA <a035604227c@stretch-builder>
2019-10-31 12:44:59 +0100

    formulario bonito

--- app/homeController.js
+++ app/homeController.js
@@ -1,10 +1,6 @@
 var miControlador = miModulo.controller(
     "homeController",
     ['$scope', '$http', function ($scope, $http) {
-<<<<<<< HEAD
-        $scope.sitio='blog';
-=======
-        $scope.controller = "homeController";
->>>>>>> b4a8fc20fdbe381790c686f4efde772ec5c1a590
+        $scope.sitio='homeController';
     }]
 )
